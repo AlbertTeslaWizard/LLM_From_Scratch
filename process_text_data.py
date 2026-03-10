be593e7a07cf31429ae9cf53f2f333550505b595
AlbertTeslaWizard <albertteslawizard@gmail.com>
2026-03-10 23:19:46 +0800

    Add text tokenization test code

--- process_text_data.py
+++ process_text_data.py
@@ -1,5 +1,16 @@
+import re
+
 with open("the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
     
-print("Total number of character:", len(raw_text))
-print(raw_text[:99])
+#print("Total number of character:", len(raw_text))
+#print(raw_text[:99])
+
+text = "Hello, world. This, is a test."
+result = re.split(r'(\s)', text)
+result2 = re.split(r'([,.]|\s)', text)
+print(result)
+print(result2)
+
+item = [x for x in result2 if x.strip()]
+print(item)
