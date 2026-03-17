768345d2b34c4bdf815c5614903841cbc4808fb6
AlbertTeslaWizard <albertteslawizard@gmail.com>
2026-03-17 22:17:31 +0800

    Add CausalAttention class

--- casual_attention.py
+++ casual_attention.py
@@ -1,5 +1,39 @@
 import torch
+import torch.nn as nn
 from trainable_self_attention_weights import SelfAttention_v2
+
+class CausalAttention(nn.Module):
+
+    def __init__(self, d_in, d_out, context_length,
+                 dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout) # New
+        self.register_buffer('mask', torch.triu(torch.ones(context_length, context_length), diagonal=1)) # New
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape # New batch dimension b
+        # For inputs where `num_tokens` exceeds `context_length`, this will result in errors
+        # in the mask creation further below.
+        # In practice, this is not a problem since the LLM (chapters 4-7) ensures that inputs  
+        # do not exceed `context_length` before reaching this forward method. 
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2) # Changed transpose
+        attn_scores.masked_fill_(  # New, _ ops are in-place
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)  # `:num_tokens` to account for cases where the number of tokens in the batch is smaller than the supported context_size
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        attn_weights = self.dropout(attn_weights) # New
+
+        context_vec = attn_weights @ values
+        return context_vec
 
 if __name__ == '__main__':
     inputs = torch.tensor(
@@ -31,6 +65,27 @@ if __name__ == '__main__':
 
     masked_simple = attention_weights * mask_simple
     print(masked_simple)
-    row_sums = masked_simple.sum(dim = -1, keepdim = True)
+    row_sums = masked_simple.sum(dim=1, keepdim=True)
     masked_simple_norm = masked_simple / row_sums
     print(masked_simple_norm)
+
+    mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+    masked = attention_scores.masked_fill(mask.bool(), -torch.inf)
+    print(masked)
+
+    attention_weights = torch.softmax(masked / keys.shape[-1] ** 0.5, dim=-1)
+    print(attention_weights)
+
+    torch.manual_seed(123)
+    dropout = torch.nn.Dropout(0.5)
+    example = torch.ones(6, 6)
+    print(dropout(example))
+    print(dropout(attention_weights))
+
+    batch = torch.stack((inputs, inputs), dim=0)
+    print(batch.shape)
+
+    torch.manual_seed(123)
+    ca = CausalAttention(d_in, d_out, context_length, 0)
+    context_vectors = ca(batch)
+    print(context_vectors)
