cc1d600c6aa8caef4c46775522efde643eddaa51
AlbertTeslaWizard <albertteslawizard@gmail.com>
2026-03-20 16:55:42 +0800

    Add LayerNorm

---
+++ LayerNorm.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+if __name__ ==  '__main__':
+    torch.manual_seed(123)
+
+    # create 2 training examples with 5 dimensions (features) each
+    batch_example = torch.randn(2, 5) 
+
+    layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+    out = layer(batch_example)
+    print(out)
+
+    mean = out.mean(dim=-1, keepdim=True)
+    var = out.var(dim=-1, keepdim=True)
+
+    out_norm = (out - mean) / torch.sqrt(var)
+    print(out_norm)
+
+    ln = LayerNorm(emb_dim=6)
+    out_ln = ln(out)
+    mean = out_ln.mean(dim=-1, keepdim=True)
+    var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+
+    print("Mean:\n", mean)
+    print("Variance:\n", var)
