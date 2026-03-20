725852e567dc6db50c8411a35a429dc016b79154
AlbertTeslaWizard <albertteslawizard@gmail.com>
2026-03-20 23:09:06 +0800

    Add GPTModel

--- FeedForward.py
+++ FeedForward.py
@@ -26,25 +26,26 @@ class FeedForward(nn.Module):
         return self.layers(x)
 
 
-gelu, relu = GELU(), nn.ReLU()
+if __name__ == '__main__':
+    gelu, relu = GELU(), nn.ReLU()
 
-# Some sample data
-x = torch.linspace(-3, 3, 100)
-y_gelu, y_relu = gelu(x), relu(x)
+    # Some sample data
+    x = torch.linspace(-3, 3, 100)
+    y_gelu, y_relu = gelu(x), relu(x)
 
-plt.figure(figsize=(8, 3))
-for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
-    plt.subplot(1, 2, i)
-    plt.plot(x, y)
-    plt.title(f"{label} activation function")
-    plt.xlabel("x")
-    plt.ylabel(f"{label}(x)")
-    plt.grid(True)
+    plt.figure(figsize=(8, 3))
+    for i, (y, label) in enumerate(zip([y_gelu, y_relu], ["GELU", "ReLU"]), 1):
+        plt.subplot(1, 2, i)
+        plt.plot(x, y)
+        plt.title(f"{label} activation function")
+        plt.xlabel("x")
+        plt.ylabel(f"{label}(x)")
+        plt.grid(True)
 
-plt.tight_layout()
-plt.savefig("activation.png")
+        plt.tight_layout()
+        plt.savefig("activation.png")
 
-ffn = FeedForward(GPT_CONFIG_124M)
-x = torch.rand(2, 3, 768) 
-out = ffn(x)
-print(out)
+    ffn = FeedForward(GPT_CONFIG_124M)
+    x = torch.rand(2, 3, 768) 
+    out = ffn(x)
+    print(out)
