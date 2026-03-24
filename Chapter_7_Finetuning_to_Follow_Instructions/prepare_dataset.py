4744cc9f945388786dd4f4dd5197d3cb61bd06a2
AlbertTeslaWizard <albertteslawizard@gmail.com>
2026-03-24 19:50:34 +0800

    Add prepare_dataset for supervised instruction finetuning

---
+++ Chapter_7_Finetuning_to_Follow_Instructions/prepare_dataset.py
@@ -0,0 +1,64 @@
+import json
+import os
+import requests
+
+def download_and_load_file(file_path, url):
+    if not os.path.exists(file_path):
+        response = requests.get(url, timeout=30)
+        response.raise_for_status()
+        text_data = response.text
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        data = json.load(file)
+    
+    return data
+
+def format_input(entry):
+    instruction_text = (
+        f"Below is an instruction that describes a task. "
+        f"Write a response that appropriately completes the request."
+        f"\n\n### Instruction:\n{entry['instruction']}"
+    )
+
+    input_text = f"\n\n### Input:\n{entry['input']}" if entry["input"] else ""
+
+    return instruction_text + input_text
+
+def format_instruction(entry):
+    model_input = format_input(entry)
+    model_response = f"\n\n### Response:\n{entry['output']}"
+    model_instruction = model_input + model_response
+    return model_instruction
+
+
+if __name__ == '__main__':
+    file_path = "instruction-data.json"
+    url = (
+        "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch"
+        "/main/ch07/01_main-chapter-code/instruction-data.json"
+    )
+
+    data = download_and_load_file("instruction-data.json", url)
+    
+    print("Number of entries:", len(data))
+    print("Example entry:\n", data[50])
+    print("Another example entry:\n", data[999])
+
+    model_input = format_input(data[50])
+    model_response = f"\n\n### Response:\n{data[50]['output']}"
+    print(format_instruction(data[50]))
+    print(format_instruction(data[999]))
+
+    train_portion = int(len(data) * 0.85)
+    test_portion = int(len(data) * 0.1)
+    val_portion = int(len(data) - train_portion - test_portion)
+
+    train_data = data[:train_portion]
+    test_data = data[train_portion:train_portion + test_portion]
+    val_data = data[train_portion+test_portion:]
+
+    print("Training set length:", len(train_data))
+    print("Validation set length:", len(val_data))
+    print("Test set length:", len(test_data))
